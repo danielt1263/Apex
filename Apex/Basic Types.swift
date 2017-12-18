e62244a5db674b19b23d9ba7bee0815cb3b993fa
Daniel Tartaglia <danielt1263@gmail.com>
2017-12-18 16:17:53 -0500

    Add `map` to Result

--- Apex/Basic Types.swift
+++ Apex/Basic Types.swift
@@ -6,10 +6,24 @@
 //  Copyright © 2017 Daniel Tartaglia. MIT License
 //
 
-
 enum Result<T> {
 	case success(T)
 	case failure(Error)
+
+	func map<U>(transform: (T) throws -> U) -> Result<U> {
+		switch self {
+		case .success(let t):
+			do {
+				return .success(try transform(t))
+			}
+			catch {
+				return .failure(error)
+			}
+		case .failure(let error):
+			return .failure(error)
+		}
+	}
 }
 
 struct UnknownError: Error { }
+
